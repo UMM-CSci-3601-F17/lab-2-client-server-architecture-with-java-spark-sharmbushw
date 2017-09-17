026ea3d43ae529d41a955ce85b81cbc7db91cec7
bushw011 <bushw011@morris.umn.edu>
2017-09-17 15:02:20 -0500

    Added test for specific todo object in FullTodoListFromDB.java

--- src/test/java/umm3601/todo/FullTodoListFromDB.java
+++ src/test/java/umm3601/todo/FullTodoListFromDB.java
@@ -24,9 +24,16 @@ public class FullTodoListFromDB {
     TodoDatabase db = new TodoDatabase("src/main/data/todos.json");
     Todo[] allTodos = db.listTodos(new HashMap<>());
     Todo firstTodo = allTodos[0];
+    assertEquals("Incorrect ID", "58895985a22c04e761776d54",firstTodo._id);
     assertEquals("Incorrect name", "Blanche", firstTodo.owner);
     assertEquals("Incorrect status", false, firstTodo.status);
     assertEquals("Incorrect body", "In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.", firstTodo.body);
     assertEquals("Incorrect category", "software design", firstTodo.category);
+    Todo secondTodo = allTodos[5];
+    assertEquals("Incorrect ID", "588959856f0b82ee93cd93eb",secondTodo._id);
+    assertEquals("Incorrect name", "Barry", secondTodo.owner);
+    assertEquals("Incorrect status", true, secondTodo.status);
+    assertEquals("Incorrect body", "Nisi sit non non sunt veniam pariatur. Elit reprehenderit aliqua consectetur est dolor officia et adipisicing elit officia nisi elit enim nisi.", secondTodo.body);
+    assertEquals("Incorrect category", "video games", secondTodo.category);
   }
 }
