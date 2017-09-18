1230bc6627fa8ffe48bd28868f5086cf25edbf34
Vipul Sharma <vipulsharma936@gmail.com>
2017-09-18 14:41:35 -0500

    Fixed issue #6

--- src/main/java/umm3601/todo/TodoDatabase.java
+++ src/main/java/umm3601/todo/TodoDatabase.java
@@ -16,10 +16,12 @@ import java.util.Map;
  * specified JSON file, and then provide various database-like
  * methods that allow the `TodoController` to "query" the "database".
  */
-public class TodoDatabase {
+public class TodoDatabase
+{
   private Todo[] allTodos;
 
-  public TodoDatabase(String userTodoFile) throws IOException {
+  public TodoDatabase(String userTodoFile) throws IOException
+  {
     Gson gson = new Gson();
     FileReader reader = new FileReader(userTodoFile);
     allTodos = gson.fromJson(reader, Todo[].class);
@@ -33,7 +35,8 @@ public class TodoDatabase {
    * @return the todo with the given ID, or null if there is no todo
    * with that ID
    */
-  public Todo getTodo(String id) {
+  public Todo getTodo(String id)
+  {
     return Arrays.stream(allTodos).filter(x -> x._id.equals(id).findFirst().orElse(null));
   }
 
@@ -43,16 +46,31 @@ public class TodoDatabase {
    * @param queryParams map of required key-value pairs for the query
    * @return an array of all the todos matching the given criteria
    */
-  public Todo[] listTodos(Map<String, String[]> queryParams) {
+  public Todo[] listTodos(Map<String, String[]> queryParams)
+  {
     Todo[] filteredTodos = allTodos;
 
     // Filter age if defined
-    if(queryParams.containsKey("owner")) {
-      String targetOwner = Integer.parseInt(queryParams.get("owner")[0])
+    if(queryParams.containsKey("owner"))
+    {
+      String targetOwner = queryParams.get("owner")[0];
       filteredTodos = filteredTodos(filteredTodos, x -> x.owner.equalIgnoreCase(targetOwner));
     }
-
-
+    if (queryParams.containsKey("category"))
+    {
+      String targetCategory = queryParams.get("category")[0];
+      filteredTodos = filteredTodos(filteredTodos, x -> x.category.equalsIgnoreCase(targetCategory));
+    }
+    if (queryParams.containsKey("status"))
+    {
+      String targetStatus = queryParams.get("status")[0];
+      filteredTodos = filteredTodos(filteredTodos, x -> x.category.equalsIgnoreCase(targetStatus));
+    }
+    if (queryParams.containsKey("contains"))
+    {
+      String targetBody = queryParams.get("contains")[0];
+      filteredTodos = filteredTodos(filteredTodos, x -> x.category.equalsIgnoreCase(targetBody);
+    }
     return filteredTodos;
   }
 }
